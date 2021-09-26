e17cc003ba96a1124f7c1c20491edeba977e2c58
Lucas Duarte <lucs.rkt@gmail.com>
2021-09-26 11:40:54 -0300

    feat(styles): added searchForm styles

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,31 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
 class SearchForm extends Component {
   render() {
     const { searchedName, onHandleChange, isDisabled, onHandleClick } = this.props;
     return (
-      <form>
-        <label htmlFor="artist-name">
-          Nome do Artista
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            name="artist-name"
-            id="artist-name"
-            onChange={ onHandleChange }
-            value={ searchedName }
-          />
-        </label>
-        <button
-          data-testid="search-artist-button"
-          disabled={ !isDisabled }
-          type="button"
-          onClick={ onHandleClick }
-        >
-          Pesquisar
-        </button>
-      </form>
+      <SearchFormStyles>
+        <form>
+          <div className="input-icon">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              name="artist-name"
+              id="artist-name"
+              onChange={ onHandleChange }
+              value={ searchedName }
+              placeholder="Nome do artista"
+              className="inp"
+            />
+            <i className="fas fa-search" />
+          </div>
+          <button
+            data-testid="search-artist-button"
+            disabled={ !isDisabled }
+            type="button"
+            onClick={ onHandleClick }
+            className="btn"
+          >
+            Pesquisar
+          </button>
+        </form>
+      </SearchFormStyles>
     );
   }
 }
@@ -36,5 +42,50 @@ SearchForm.propTypes = {
   isDisabled: PropTypes.bool.isRequired,
   onHandleClick: PropTypes.func.isRequired,
 };
+
+const SearchFormStyles = styled.div`
+  margin: 80px auto;
+  width: fit-content;
+  form {
+    width: 38rem;
+    display: flex;
+    margin-bottom: 2rem;
+  }
+
+  .btn {
+    width: 8.375rem;
+    background-color: #003BE5;
+    color: white;
+    font-weight: bold;
+    font-size: 1rem;
+    padding: .5rem;
+    border: none;
+    border-radius: .25rem;
+    height: 3rem;
+    transition: .7s;
+  }
+
+  .btn:hover {
+    background-color: #002AA3;
+    cursor: pointer;
+  }
+
+  .inp {
+    width: 32rem;
+    padding: .5rem 5px;
+    border: none;
+  }
+
+  .input-icon {
+    display: flex;
+    align-items: center;
+    padding: 5px;
+    border: 1px solid #bbbbbb;
+    border-radius: .25rem;
+    margin-right: 1.5rem;
+    color: #BBBBBB;
+
+  }
+`;
 
 export default SearchForm;
