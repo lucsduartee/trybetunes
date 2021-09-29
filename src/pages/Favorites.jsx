d48cbcebed337901d57a16392c40b68c85ccb19a
Lucas Duarte <lucs.rkt@gmail.com>
2021-09-29 13:57:31 -0300

    feat(styles): finished styles

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import Header from '../components/Header';
 import Loading from '../components/Loading';
 import MusicCard from '../components/MusicCard';
@@ -43,15 +44,19 @@ class Favorites extends Component {
   renderMusicCard = (favorites) => {
     const isTrue = true;
     return (
-      <section>
+      <section className="favorites">
+        <h1>Músicas Favoritas:</h1>
         {
           favorites.map((favorite) => (
-            <MusicCard
-              onChange={ this.handleChange }
-              music={ favorite }
-              key={ favorite.trackName }
-              isFavorite={ isTrue }
-            />
+            <div className="card-favorite" key={ favorite.trackName }>
+              <img src={ favorite.artworkUrl100 } alt="asdasd" />
+              <MusicCard
+                onChange={ this.handleChange }
+                music={ favorite }
+                isFavorite={ isTrue }
+              />
+              {console.log(favorite)}
+            </div>
           ))
         }
       </section>
@@ -61,12 +66,44 @@ class Favorites extends Component {
   render() {
     const { state: { isLoading, favorites } } = this;
     return (
-      <div data-testid="page-favorites">
-        <Header />
-        { isLoading ? <Loading /> : this.renderMusicCard(favorites) }
-      </div>
+      <FavoritesStyles>
+        <div data-testid="page-favorites">
+          <Header />
+          { isLoading ? <Loading /> : this.renderMusicCard(favorites) }
+        </div>
+      </FavoritesStyles>
     );
   }
 }
+
+const FavoritesStyles = styled.div`
+  .favorites {
+    width: 40%;
+    margin: 100px auto;
+  }
+
+  .favorites h1 {
+    margin-bottom: 35px;
+    display: block;
+    color: #001813;
+    font-size: 1.75rem;
+    font-family: 'Epilogue', sans-serif;
+  }
+
+  .card-favorite {
+    display: flex;
+    background: #E1E5EB;
+    justify-content: space-around;
+    margin-bottom: 40px;
+    border-radius: 10px;
+    padding: 10px;
+    box-shadow: 3px 3px 10px 2px rgba(0, 0, 0, 0.3);
+  }
+
+  .card-favorite img {
+    width: 120px;
+    border-radius: 50%;
+  }
+`;
 
 export default Favorites;
